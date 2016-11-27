642585796b21d4d1f9e5931267b30b8f1154b879
Will Stone <will@eggbox.io>
2016-11-27 09:14:31 +0000

    redirect on just album

--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ import App from './containers/App'; // wrapper
 import Albums from './containers/Albums';
 
 // History without ?_k=
-import { Route, Router, useRouterHistory, IndexRoute } from 'react-router';
+import { Route, Router, useRouterHistory, IndexRoute, Redirect } from 'react-router';
 import { createHashHistory } from 'history';
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 ReactDOM.render(
   <Router history={appHistory}>
+    <Redirect from="album" to="/"/>
     <Route component={App} path="/">
       <IndexRoute component={Albums}/>
       <Route path="/album/:album" component={Albums}/>
